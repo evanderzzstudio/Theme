24b67bd6254509fbdca0c6a5f43be2a9d336ea48
evanderzzstudio <evanderstudio@gmail.com>
2023-12-04 16:55:06 +0700

    - Update clock styling and display.

--- script.js
+++ script.js
@@ -29,4 +29,10 @@ function setTime() {
     const minutes = time.getMinutes()
     const seconds = time.getSeconds() \
     const ampm = hours >= 12 ? "PM" : "AM"
+
+    hourEl.style.transform = "translate(-50%,-100%) rotate(${scale(hoursForClock,0,12,0,360)}deg)"
+    minuteEl.style.transform = "translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)"
+    secondEl.style.transform = "translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)"
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
